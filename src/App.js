911deb90cff0fbeb1cc420795ef920ca31af66eb
haein.ha <haein8741@gmail.com>
2021-01-12 22:41:55 +0900

    20210112 before css

--- src/App.js
+++ src/App.js
@@ -1,79 +1,41 @@
 import React from 'react'
 import PropTypes from "prop-types"
+import axios from "axios";
+import Movie from './Movie';
 
-// function Food(props){
-//   console.log(props)
-//   return <h1>I like Potato</h1>
-// }
+class App extends React.Component{
+  state ={
+    isLoading : true,
+    movies: []
+  };
 
-/*
-friends=["dal","mark","lynn"]
-friends.map(function(current){
-  console.log(current);
-  return 0
-})
-
-friends.map(current=>{
-  console.log(current);
-  return 0
-})
-*/
-
-function Food({name,picture,rating}){
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>rating {rating}/5</h4>
-    <img src={picture}/>
-  </div>
-}
-
-Food.propTypes={
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired
-
-}
-
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating:5
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating:5
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating:5
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating:5
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating:5
+  getMovies = async () => {
+    const { data: {
+      data: {movies}
+    }
+  }=await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+  this.setState({movies,isLoading: false});
   }
-];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      {foodILike.map(dish=><Food name={dish.name} picture={dish.image} rating={dish.rating}/>)}
-    </div>
-  );
+  componentDidMount(){
+    this.getMovies()
+  }
+// async componentDidMount(){
+// const movies=axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+// }
+  render(){
+    const {isLoading, movies} = this.state;
+    //return <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>
+    return <div>{isLoading ? "Loading.." : movies.map(movie => {
+      console.log(movie);
+      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />;
+    })}</div>
+
+    // return <div>{isLoading ? "Loading.." : movies.map(movie => (
+    //   <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />;
+    // )}</div>
+  };
+
 }
 
 export default App;
